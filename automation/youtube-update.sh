6972360e5107be22849c65f866e97eb08aff815f
Jake Lee <github@jakelee.co.uk>
2023-09-09 14:54:07 +0100

    Extract all paths

--- automation/youtube-update.sh
+++ automation/youtube-update.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+channels_file="${WORKSPACE}/automation/channels.txt"
+template_file="${WORKSPACE}/automation/template.md"
+output_file="${WORKSPACE}/README.md"
+
 header_prefix="#### "
 placeholder_text="dynamic-channel-data"
+temp_output_file="output.json"
 output=""
 
 # Convert list of channels into Markdown tables
@@ -15,27 +20,27 @@ while read -r line; do
         echo "Adding channel ${channel_name} (${channel_id})"
         curl "https://youtube.googleapis.com/youtube/v3/channels?part=statistics,snippet&id=${channel_id}&key=${API_KEY}" \
             --header 'Accept: application/json' \
-            -fsSL -o output.json
+            -fsSL -o ${temp_output_file}
 
         # Pull channel data out of response if possible
         if [[ $(jq -r '.pageInfo.totalResults' output.json) == 1 ]]; then
-            title=$(jq -r '.items[0].snippet.title' output.json)
-            url=$(jq -r '.items[0].snippet.customUrl' output.json)
-            video_count=$(jq -r '.items[0].statistics.videoCount' output.json | numfmt --to=si)
-            subscriber_count=$(jq -r '.items[0].statistics.subscriberCount' output.json | numfmt --to=si)
-            view_count=$(jq -r '.items[0].statistics.viewCount' output.json | numfmt --to=si)
+            title=$(jq -r '.items[0].snippet.title' ${temp_output_file})
+            url=$(jq -r '.items[0].snippet.customUrl' ${temp_output_file})
+            video_count=$(jq -r '.items[0].statistics.videoCount' ${temp_output_file} | numfmt --to=si)
+            subscriber_count=$(jq -r '.items[0].statistics.subscriberCount' ${temp_output_file} | numfmt --to=si)
+            view_count=$(jq -r '.items[0].statistics.viewCount' ${temp_output_file} | numfmt --to=si)
             echo "Added ${title}: ${video_count} videos (${view_count} views)"
             output="${output}| ${emoji}[${title}](https://youtube.com/${url}) | ${video_count} | ${subscriber_count} | ${view_count} |\n"
         else
-            echo "Failed! Bad response received: $(<output.json)"
+            echo "Failed! Bad response received: $(<${temp_output_file})"
             exit 1
         fi
     fi
-done < "${WORKSPACE}/automation/channels.txt"
+done < ${channels_file}
 
 # Replace placeholder in template with output, updating the README
-template_contents=$(<"${WORKSPACE}/automation/template.md")
-echo -e "${template_contents//${placeholder_text}/${output}}" > "${WORKSPACE}/README.md"
+template_contents=$(<${template_file})
+echo -e "${template_contents//${placeholder_text}/${output}}" > ${output_file}
 
 # Debug
 cat "${WORKSPACE}/README.md"
